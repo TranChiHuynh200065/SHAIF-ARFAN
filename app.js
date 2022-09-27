8c882aa29fa3859a62a18fa7fa232f5dcb72a238
TranChiHuynh200065 <huynhtcgcc200065@fpt.edu.vn>
2022-09-27 09:46:45 +0700

    Huynh

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-    //__dirname : It will resolve to your project folder.
-});
-
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
-
-
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
